a943cf538de010cea3d6e09e9376aa1f75b074f3
hyTONG2 <1548327816@qq.com>
2022-06-21 13:59:48 +0000

    new

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,   //去除右上角的debug标志
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
@@ -26,7 +27,19 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('ConstrainedBox、SizedBox(HYT)'),
+        title: Text('流式布局Flow(HYT)'),
+        /*actions:<Widget>[
+          UnconstrainedBox(
+          child: SizedBox(
+            width:20,
+            height:20,
+            child:CircularProgressIndicator(
+              strokeWidth:3,
+              valueColor:AlwaysStoppedAnimation(Colors.white70),
+            ),
+          )
+          )
+        ]*/
       ),
       body: MyHomeContent(),
     );
@@ -50,6 +63,7 @@ class _MyHomeContentState extends State<MyHomeContent> {
         /*const Text(
           'HAHAHAHA好ok',
         ),*/
+
         /*ConstrainedBox(
           constraints:
               BoxConstraints(minWidth: double.infinity, minHeight: 50.0),
@@ -58,12 +72,93 @@ class _MyHomeContentState extends State<MyHomeContent> {
             child: redBox,
           ),
         ),*/
-        SizedBox(
-            width: 80.0,
-            height: 80.0,
-          child:redBox,
-        ),
+
+        /*Wrap(
+          spacing: 8.0, // 主轴(水平)方向间距
+          runSpacing: 4.0, // 纵轴（垂直）方向间距
+          alignment: WrapAlignment.center, //沿主轴方向居中
+          children: <Widget>[
+            Chip(
+              avatar: CircleAvatar(backgroundColor: Colors.blue, child: Text('A')),
+              label: Text('Hamilton'),
+            ),
+            Chip(
+              avatar: CircleAvatar(backgroundColor: Colors.blue, child: Text('M')),
+              label: Text('Lafayette'),
+            ),
+            Chip(
+              avatar: CircleAvatar(backgroundColor: Colors.blue, child: Text('H')),
+              label: Text('Mulligan'),
+            ),
+            Chip(
+              avatar: CircleAvatar(backgroundColor: Colors.blue, child: Text('J')),
+              label: Text('Laurens'),
+            ),
+          ],
+        ),*/
+
+        //对六个色块进行自定义流式布局：
+        Flow(
+          delegate: TestFlowDelegate(margin: EdgeInsets.all(10.0)),
+          children: <Widget>[
+            Container(width: 80.0, height:80.0, color: Colors.red,),
+            Container(width: 80.0, height:80.0, color: Colors.green,),
+            Container(width: 80.0, height:80.0, color: Colors.blue,),
+            Container(width: 80.0, height:80.0,  color: Colors.yellow,),
+            Container(width: 80.0, height:80.0, color: Colors.brown,),
+            Container(width: 80.0, height:80.0,  color: Colors.purple,),
+          ],
+        )
+        /*SizedBox(
+          width: 80.0,
+          height: 80.0,
+          child: redBox,
+        ),*/
+
       ],
     );
   }
 }
+
+class TestFlowDelegate extends FlowDelegate {
+  EdgeInsets margin;
+
+  TestFlowDelegate({this.margin = EdgeInsets.zero});
+
+  double width = 0;
+  double height = 0;
+
+  @override
+  void paintChildren(FlowPaintingContext context) {
+    var x = margin.left;
+    var y = margin.top;
+    //计算每一个子widget的位置
+    for (int i = 0; i < context.childCount; i++) {
+      var w = context.getChildSize(i)!.width + x + margin.right;
+      if (w < context.size.width) {
+        context.paintChild(i, transform: Matrix4.translationValues(x, y, 0.0));
+        x = w + margin.left;
+      } else {
+        x = margin.left;
+        y += context.getChildSize(i)!.height + margin.top + margin.bottom;
+        //绘制子widget(有优化)
+        context.paintChild(i, transform: Matrix4.translationValues(x, y, 0.0));
+        x += context.getChildSize(i)!.width + margin.left + margin.right;
+      }
+    }
+  }
+
+  @override
+  Size getSize(BoxConstraints constraints) {
+    // 指定Flow的大小，简单起见我们让宽度竟可能大，但高度指定为200，
+    // 实际开发中我们需要根据子元素所占用的具体宽高来设置Flow大小
+    return Size(double.infinity, 200.0);
+  }
+
+  @override
+  bool shouldRepaint(FlowDelegate oldDelegate) {
+    return oldDelegate != this;
+  }
+}
+
+
